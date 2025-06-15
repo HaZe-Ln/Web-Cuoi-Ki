64928be4c94e50f0bbc85bf09588573ee68b855a
HaZe-Ln <anhhoang24050303@gmail.com>
2025-06-15 19:08:11 +0700

    fix

--- Đồ án cuối kỳ/ve_chung_toi.js
+++ Đồ án cuối kỳ/ve_chung_toi.js
@@ -1,7 +1,21 @@
-document.getElementById("dichVuBtn").addEventListener("click", function () {
+    document.addEventListener('DOMContentLoaded', function() {
+    var btn = document.getElementById('accountBtn');
+    var menu = btn.parentElement;
+    btn.onclick = function(e) {
+      e.stopPropagation();
+      menu.classList.toggle('open');
+    };
+    document.addEventListener('click', function() {
+      menu.classList.remove('open');
+    });
+  });
+
+  document.getElementById("dichVuBtn").addEventListener("click", function () {
   const dropdown = document.getElementById("dichVuDropdown");
   dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
 });
+
+// Ẩn dropdown khi click ra ngoài
 document.addEventListener("click", function (event) {
   const dichVuBtn = document.getElementById("dichVuBtn");
   const dichVuDropdown = document.getElementById("dichVuDropdown");
